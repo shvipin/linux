22436f8569af80c50ea85b60c0707c2f85841365
Vipin Sharma <vipinsh@google.com>
2026-05-11 16:47:47 -0700

    vfio/pci: Register a file handler with Live Update Orchestrator
    Register a live update file handler for vfio-pci device files. Add stub
    implementations of all required callbacks so that registration does not
    fail (i.e. to avoid breaking git-bisect). Register it with both live
    update framework and PCI subsystem.
    
    This file handler will be extended in subsequent commits to enable a
    device bound to vfio-pci to run without interruption while the host is
    going through a kexec Live Update.
    
    Put this support behind a new Kconfig VFIO_PCI_LIVEUPDATE that is marked
    experimental and default-disabled until more of the device preservation
    support has landed in the kernel.
    
    Co-developed-by: David Matlack <dmatlack@google.com>
    Signed-off-by: David Matlack <dmatlack@google.com>
    Signed-off-by: Vipin Sharma <vipinsh@google.com>

--- drivers/vfio/pci/vfio_pci.c
+++ drivers/vfio/pci/vfio_pci.c
@@ -264,10 +264,14 @@ static int __init vfio_pci_init(void)
 
 	vfio_pci_core_set_params(nointxmask, is_disable_vga, disable_idle_d3);
 
+	ret = vfio_pci_liveupdate_init();
+	if (ret)
+		return ret;
+
 	/* Register and scan for devices */
 	ret = pci_register_driver(&vfio_pci_driver);
 	if (ret)
-		return ret;
+		goto err_liveupdate_cleanup;
 
 	vfio_pci_fill_ids();
 
@@ -275,12 +279,17 @@ static int __init vfio_pci_init(void)
 		pr_warn("device denylist disabled.\n");
 
 	return 0;
+
+err_liveupdate_cleanup:
+	vfio_pci_liveupdate_cleanup();
+	return ret;
 }
 module_init(vfio_pci_init);
 
 static void __exit vfio_pci_cleanup(void)
 {
 	pci_unregister_driver(&vfio_pci_driver);
+	vfio_pci_liveupdate_cleanup();
 }
 module_exit(vfio_pci_cleanup);
 
